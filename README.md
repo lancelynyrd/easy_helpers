ae5dd536356fde5c5719fb6dbb54f6e69f6a120d
JaeHo Song <thruthesky@gmail.com>
2024-07-10 16:59:11 +0800

    first release

---
+++ README.md
@@ -0,0 +1,13 @@
+# Easy Helpers
+
+
+Helper classes, functions, and extensions for FireFlutter
+
+
+
+## Packages
+
+
+- device local to get the locale of the device
+
+- intl
---
+++ lib/easy_helpers.dart
@@ -0,0 +1,12 @@
+export './extensions/datetime.extension.dart';
+export './extensions/string.extensions.dart';
+
+export './functions/alert_dialog.function.dart';
+export './functions/confirm_dialog.function.dart';
+export './functions/error_dialog.function.dart';
+export './functions/input_dialog.function.dart';
+export './functions/locale.functions.dart';
+export './functions/log.functions.dart';
+export './functions/platform.functions.dart';
+export './functions/string.functions.dart';
+export './functions/toast.function.dart';
